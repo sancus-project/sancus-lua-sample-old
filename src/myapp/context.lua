9ed0cfae2a2cd3faf2d5095e2a32ddc6dda92956
Alejandro Mery <amery@geeks.cl>
2011-05-26 22:52:14 +0200

    myapp.context: Introduced factory for the initial context used by the handlers and passed to the renderer

---
+++ src/myapp/context.lua
@@ -0,0 +1,7 @@
+--
+
+module (...)
+
+function Context(wsapi_env)
+	return {}
+end
--- src/myapp/handler/index.lua
+++ src/myapp/handler/index.lua
@@ -1,18 +1,19 @@
 --
 
 require "myapp.conf.common"
+require "myapp.context"
 require "myapp.html"
 
 local urls = myapp.conf.common.urls
+local Context = myapp.conf.Context
 local render_html = myapp.html.render
 
 module (...)
 
 local function handler(wsapi_env)
-	local d = {
-		who = "world",
-	}
-	return render_html('_index')
+	local c = Context(wsapi_env)
+	c.who = "world",
+	return render_html('_index', c)
 end
 
 urls:add("/[index.{format:html}]$", handler)
