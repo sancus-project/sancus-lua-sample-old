0d5222e7963ec3535f4b63d0d6b0f1b34774dbda
Alejandro Mery <amery@geeks.cl>
2011-05-29 12:14:15 +0200

    myapp.handler.index: changed to use Resource

--- src/myapp/handler/index.lua
+++ src/myapp/handler/index.lua
@@ -1,19 +1,24 @@
 --
 
+require "sancus.resource"
+
 require "myapp.conf.common"
 require "myapp.context"
 require "myapp.html"
 
 local urls = myapp.conf.common.urls
 local Context = myapp.conf.Context
+local Resource = sancus.resource.Resource
 local render_html = myapp.html.render
 
 module (...)
 
-local function handler(wsapi_env)
+local R = Resource()
+
+function R:GET(wsapi_env)
 	local c = Context(wsapi_env)
 	c.who = "world",
 	return render_html('_index', c)
 end
 
-urls:add("/[index.{format:html}]$", handler)
+urls:add("/[index.{format:html}]$", R)
